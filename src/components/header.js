e5dd1440cb47439f616addd42dc5c03362f358b2
myusername <himashawije5678@gmail.com>
2024-03-13 16:30:24 +0530

    first-commit

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+function Header() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className="cover-poster-item">
+            <img
+              src="https://sm.ign.com/t/ign_in/screenshot/default/marv_eawn.1200.png"
+              className="d-block w-10’0"
+              alt="vidly-cover-1"
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row className="title-section">
+        <Col className="title-text">Vidly</Col>
+      </Row>
+    </Container>
+  );
+}
+export default Header;
